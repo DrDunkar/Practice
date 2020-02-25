7edcd7b1f3773974fad7afec62d4072556d8b7c4
RajBharat <bkcraaj@gmail.com>
2020-02-25 12:49:54 +0545

    list view and detail view  add

---
+++ generic_view/restro/views.py
@@ -0,0 +1,16 @@
+from django.views import generic
+from .models import Resturent
+from .models import Food_Item
+class IndexView(generic.ListView):
+    model = Resturent
+    template_name = 'restro/index.html'
+
+    def get_queryset(self):
+        return Resturent.objects.all()
+
+class FoodDetailView(generic.DetailView):
+    model = Resturent
+    template_name = 'restro/detail.html'        
+
+    # def get_queryset(self):
+    #     return Food_Item.objects.all()
